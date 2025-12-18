d3099ac7fa66292d6fba3be4f34cc89ee99553f9
HenriqueBaetaLeite <hbaetaleite@gmail.com>
2025-12-18 19:59:57 -0300

    link na pagina home

--- app/page.js
+++ app/page.js
@@ -1,9 +1,12 @@
-import Image from "next/image";
+import Link from "next/link";
 
 export default function Home() {
   return (
     <div className="flex min-h-screen items-center justify-center bg-zinc-50 font-sans dark:bg-black">
       <h1>Siga para /votacao</h1>
+      <Link href="/votacao" className="ml-4 text-blue-600 underline">
+        Ir para votação
+      </Link>
     </div>
   );
 }
